cd5d9870ca750110d42fa1be1da6c0159f3d7a50
abhinaylakhera <abhinaylakherashivpuri@gmail.com>
2025-01-02 00:44:43 +0530

    commot by mian

--- src/MM/newclass.java
+++ src/MM/newclass.java
@@ -2,11 +2,6 @@ package MM;
 
 public class newclass extends First {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello jiii");
-		System.out.println("Hello ji");
-
-	}
+	
 
 }
