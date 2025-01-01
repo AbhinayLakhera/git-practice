e819b419c65577355c0641726e061185ead4eca9
abhinaylakhera <abhinaylakherashivpuri@gmail.com>
2025-01-02 00:31:35 +0530

    new class added

---
+++ src/MM/newclass.java
@@ -0,0 +1,12 @@
+package MM;
+
+public class newclass extends First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello jiii");
+		System.out.println("Hello ji");
+
+	}
+
+}
